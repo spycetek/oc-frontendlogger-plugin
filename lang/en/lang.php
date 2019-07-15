0ef7a3a890e865abc93d4dff5527c0e595a899ce
Ryan Viajedor <viajedor.ryan1990@gmail.com>
2019-07-15 13:36:01 +0800

    Edit plugin.yaml file.

--- lang/en/lang.php
+++
@@ -1,6 +0,0 @@
-<?php return [
-    'plugin' => [
-        'name' => 'Frontend Logger',
-        'description' => 'This is an OctoberCMS plugin that helps developer to record frontend logs.'
-    ]
-];
